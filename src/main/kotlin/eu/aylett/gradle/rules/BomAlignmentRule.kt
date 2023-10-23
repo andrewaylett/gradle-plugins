2b37408e55da717cdcb6aa6ee76845bc85039917
Andrew Aylett <andrew@aylett.co.uk>
2023-10-23 20:20:08 +0100

    Pre-merge tidy, hopefully makes the pipe green
    Signed-off-by: Andrew Aylett <andrew@aylett.co.uk>

--- src/main/kotlin/eu/aylett/gradle/rules/BomAlignmentRule.kt
+++ src/main/kotlin/eu/aylett/gradle/rules/BomAlignmentRule.kt
@@ -26,22 +26,23 @@ import org.gradle.api.artifacts.ComponentMetadataRule
 class BomAlignmentRule : ComponentMetadataRule {
   override fun execute(ctx: ComponentMetadataContext) {
     val details = ctx.details
-    val id = details.id
-    val group = id.group
+    val group = details.id.group
+    val name = details.id.name
+    val version = details.id.version
     if (group.startsWith("org.mockito")) {
-      details.belongsTo("org.mockito:mockito-virtual-bom:${id.version}")
+      details.belongsTo("org.mockito:mockito-virtual-bom:$version")
     } else if (group.startsWith("io.dropwizard.metrics")) {
-      details.belongsTo("io.dropwizard.metrics:metrics-virtual-bom:${id.version}")
+      details.belongsTo("io.dropwizard.metrics:metrics-virtual-bom:$version")
     } else if (group == "io.dropwizard") {
-      details.belongsTo("io.dropwizard:dropwizard-virtual-bom:${id.version}")
+      details.belongsTo("io.dropwizard:dropwizard-virtual-bom:$version")
     } else if (group.startsWith("org.glassfish.jersey")) {
-      details.belongsTo("org.glassfish.jersey:jersey-virtual-bom:${id.version}")
-    } else if (group.startsWith("org.jetbrains.kotlin")) {
-      details.belongsTo("org.jetbrains.kotlin:kotlin-virtual-bom:${id.version}")
-    } else if (group == "org.assertj") {
-      details.belongsTo("org.assertj:assertj-virtual-bom:${id.version}")
-    } else if (group == "com.google.protobuf") {
-      details.belongsTo("com.google.protobuf:proto-virtual-bom:${id.version}")
+      details.belongsTo("org.glassfish.jersey:jersey-virtual-bom:$version")
+    } else if (group.equals("org.jetbrains.kotlin") && name.startsWith("kotlin-")) {
+      details.belongsTo("org.jetbrains.kotlin:kotlin-virtual-bom:$version")
+    } else if (group.equals("org.assertj")) {
+      details.belongsTo("org.assertj:assertj-virtual-bom:$version")
+    } else if (group.equals("com.google.protobuf")) {
+      details.belongsTo("com.google.protobuf:proto-virtual-bom:$version")
     }
   }
 }
