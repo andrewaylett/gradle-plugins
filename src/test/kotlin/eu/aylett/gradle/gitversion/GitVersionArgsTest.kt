4ad2350fd5d7fd5f46345d2674360c7462fc168e
Andrew Aylett <andrew@aylett.co.uk>
2023-11-22 21:54:51 +0000

    More tests for GitVersionArgs
    Signed-off-by: Andrew Aylett <andrew@aylett.co.uk>

--- src/test/kotlin/eu/aylett/gradle/gitversion/GitVersionArgsTest.kt
+++ src/test/kotlin/eu/aylett/gradle/gitversion/GitVersionArgsTest.kt
@@ -16,6 +16,11 @@
  */
 package eu.aylett.gradle.gitversion
 
+import org.gradle.api.GradleException
+import org.gradle.testfixtures.ProjectBuilder
+import org.hamcrest.MatcherAssert.assertThat
+import org.hamcrest.Matchers.equalTo
+import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.DynamicTest
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.TestFactory
@@ -45,5 +50,74 @@ class GitVersionArgsTest {
   @Test
   fun require_dash_or_at_symbol_at_prefix_end() {
     assertThrows<IllegalStateException> { GitVersionArgs("v") }
+  }
+
+  data class Arg(val arg: Array<Any?>, val prefix: String) {
+    override fun equals(other: Any?): Boolean {
+      if (this === other) return true
+      if (javaClass != other?.javaClass) return false
+
+      other as Arg
+
+      return arg.contentEquals(other.arg)
+    }
+
+    override fun hashCode(): Int {
+      return arg.contentHashCode()
+    }
+  }
+
+  @TestFactory
+  fun `can pass different types of args to make an argument object`(): Stream<DynamicTest> {
+    val p = ProjectBuilder.builder().build().providers
+    return DynamicTest.stream(
+      Stream.of(
+        Arg(arrayOf(), ""),
+        Arg(arrayOf(mapOf<Any, Any>("prefix" to "")), ""),
+        Arg(arrayOf(mapOf<Any, Any>("prefix" to "foo@")), "foo@"),
+        Arg(arrayOf(emptyMap<Any, Any>()), ""),
+        Arg(arrayOf(""), ""),
+        Arg(arrayOf("foo@"), "foo@"),
+        Arg(arrayOf(p.provider { "" }), ""),
+        Arg(arrayOf(p.provider { "foo@" }), "foo@"),
+      ),
+      { arg -> "Array '${arg.arg.contentToString()}', Prefix '${arg.prefix}'" },
+      { arg ->
+        val r = GitVersionArgs.fromGroovyClosure(*arg.arg)
+        assertThat(r.prefix, equalTo(arg.prefix))
+      },
+    )
+  }
+
+  @TestFactory
+  fun `fails with multiple arguments`(): Stream<DynamicTest> {
+    val p = ProjectBuilder.builder().build().providers
+    return DynamicTest.stream(
+      Stream.of(
+        arrayOf(mapOf<Any, Any>("prefix" to "", "bar" to "baz")),
+        arrayOf(mapOf<Any, Any>("prefix" to "foo@", "bar" to "baz")),
+        arrayOf("", emptyMap<Any, Any>()),
+        arrayOf("", ""),
+        arrayOf("foo@", ""),
+        arrayOf("", p.provider { "" }),
+        arrayOf(p.provider { "foo@" }, ""),
+      ),
+      { arg -> "Array '${arg.contentToString()}'" },
+      { arg ->
+        assertThrows<IllegalArgumentException> { GitVersionArgs.fromGroovyClosure(*arg) }
+      },
+    )
+  }
+
+  companion object {
+    @JvmStatic
+    @BeforeAll
+    fun `ensure global services is initialised`() {
+      try {
+        ProjectBuilder.builder().build()
+      } catch (e: GradleException) {
+        assertThat(e.message, equalTo("Could not inject synthetic classes."))
+      }
+    }
   }
 }
